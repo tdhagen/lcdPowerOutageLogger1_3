20eaeb3ae69910e83ef86d6d346bdd6eee835908
tdhagen <thomas.d.hagen@gmail.com>
2018-04-17 21:31:57 -0400

    Add files via upload
    V1_3 corrects sunrise and sunset time to comprehend DST properly, and slows the rate that the display alternates between sunrise/sunset/date.

---
+++ lcdPowerOutageLogger1_3.ino
@@ -0,0 +1,47 @@
+/*----------------------------------------------------------------------*
+ * Arduino sketch to implement a Power Outage Logger using the          *
+ * Microchip MCP79412 RTC. Logs up to 7 outages (power down/up times)   *
+ * in the RTC's SRAM.                                                   *
+ *                                                                      *
+ * A circuit schematic and PC board for this project is available at    *
+ * https://github.com/JChristensen/PowerOutageMonitor_HW                *
+ *                                                                      *
+ * Jack Christensen 23Aug2012 v1.0                                      *
+ * T Hagen 21Jan2013 v1.1 - sunrise/sunset added                        *
+ * T Hagen 16Oct2014 v1.2 - corrected leading 0 on sunset.              *
+ *   Binary sketch size: 22,366 bytes                                   *
+ * T Hagen 09Mar2018 v1.3 - Chg sunrise/set display times to 3 seconds  *
+ *   Changed location to Davison.  rise/set is now updated to current   *
+ *   daylight/savings time                                              *
+ *                                                                      *
+ * The normal display is a clock showing time, date, and the number of  *
+ * power outages logged in angle brackets, e.g. <4>. After a new power  *
+ * outage, the alert LED is illuminated. Viewing the outage log turns   *
+ * the LED off. The clock adjusts automatically for daylight saving     *
+ * time.                                                                *
+ *                                                                      *
+ * From the clock display:                                              *
+ *                                                                      *
+ * (1) Press UP or DN to view the outage log. Pressing UP will show     *
+ * the first (earliest) outage, pressing UP again will show the next    *
+ * outage. Pressing DN will show last (most recent) outage, pressing    *
+ * DN again will show the previous outage. Press SET to return to clock *
+ * mode, or it will automatically return after 30 seconds.              *
+ *                                                                      *
+ * (2) Press SET to begin the set sequence. Press UP and DN to adjust   *
+ * each parameter, hold to adjust rapidly. Press SET to advance to the  *
+ * next parameter. Hold SET to cancel the set sequence. Pressing UP     *
+ * and DN simultaneously while setting either seconds or the RTC        *
+ * calibration will zero the value.                                     *
+ *                                                                      *
+ * (3) From clock mode or while viewing the outage log,                 *
+ * hold SET to clear the outage log.                                    *
+ *                                                                      *
+ * This work is licensed under the Creative Commons Attribution-        *
+ * ShareAlike 3.0 Unported License. To view a copy of this license,     *
+ * visit http://creativecommons.org/licenses/by-sa/3.0/ or send a       *
+ * letter to Creative Commons, 171 Second Street, Suite 300,            *
+ * San Francisco, California, 94105, USA.                               *
+ *----------------------------------------------------------------------*/
+
+
---
+++ util.ino
@@ -0,0 +1,124 @@
+//print time to Serial or lcd
+void printTime(Print &p, time_t t)
+{
+  printI00(p, hour(t), ':');
+  printI00(p, minute(t), ':');
+  printI00(p, second(t), ' ');
+}
+//--------------------------------------------------------------
+//print date to Serial or lcd
+void printDate(Print &p, time_t t)
+{
+  p << dayShortStr(weekday(t)) << ' ';
+  printI00(p, day(t), ' ');
+  p << monthShortStr(month(t)) << ' ' << _DEC(year(t));
+}
+//--------------------------------------------------------------
+//Print an integer in "00" format (with leading zero),
+//followed by a delimiter character to Serial or lcd.
+//Input value assumed to be between 0 and 99.
+void printI00(Print &p, int val, char delim)
+{
+  if (val < 10) p << '0';
+  p << _DEC(val) << delim;
+  return;
+}
+//--------------------------------------------------------------
+//print date, time and number of outages logged to the lcd
+void lcdDateTime(void)
+{
+  lcd.setCursor(0, 0);
+  printTime(lcd, local);
+  lcd << tcr -> abbrev;
+  lcd.setCursor(0, 1);  // col,row
+
+  // use local rather than utc, or day will be off part of the time
+  d=ordinalDate(year(local), month(local), day(local) );
+  {
+    if(loopCount <=3)  // was ==0
+    {
+      printDate(lcd, local);
+    }
+    else
+      if((loopCount <=6) && (loopCount >3))  //  was ==1
+      {   
+
+        timeOffset = (tcr -> offset)/60;  //  this obtains the current offset value for the current timechange rule
+        //    calcSunset (d, LAT, LONG, false, UTC_OFFSET, OFFICIAL_ZENITH, ohour, ominutes);
+        calcSunset (d, LAT, LONG, false, timeOffset, OFFICIAL_ZENITH, ohour, ominutes);
+        lcd.setCursor(0,1);  // c, r
+        lcd.print("Sunrise "); 
+        if(ohour<12) 
+        {
+          lcd.print (ohour);  
+        }
+        else
+        {
+          lcd.print (ohour -12);
+        }  
+        lcd.print (":");
+        if(ominutes <10)
+        {
+          lcd.print("0");
+        }
+        lcd.print (ominutes);
+        lcd.print(" AM");
+      }
+      else
+        if((loopCount <=9) && (loopCount >6))  // was ==2
+        {
+          //    calcSunset (d, LAT, LONG, true, UTC_OFFSET, OFFICIAL_ZENITH, ohour, ominutes);
+          calcSunset (d, LAT, LONG, true, timeOffset, OFFICIAL_ZENITH, ohour, ominutes);
+          lcd.setCursor(0,1);  // c, r
+          lcd.print("Sunset  "); 
+          if(ohour<12) 
+          {
+            lcd.print (ohour);  
+          }
+          else
+          {
+            lcd.print (ohour -12);     // need to add the ,10 add digit here
+          }
+          lcd.print (":");  // added to give leading zero v1.2
+          if(ominutes <10)
+          {
+            lcd.print("0");
+          }
+          lcd.print (ominutes);  //  end of add v1.2
+          lcd.print(" PM");
+        }
+  }
+  //display the number of outages logged
+  lcd.setCursor(13, 0);
+  if (nOutage > 0)
+    lcd << '<' << _DEC(nOutage) << '>';
+  else
+    lcd << F("   ");
+}
+
+//--------------------------------------------------------------
+//adjust lcd brightness
+void brAdjust(void)
+{
+  int br;
+  int pc = photoCell.reading(analogRead(PHOTOCELL_PIN));
+
+  br = map(constrain(pc, 50, 550), 50, 550, 10, 1);  //750 was 550
+  analogWrite(BACKLIGHT_PIN, br * 255 / 10);
+}
+
+//Leap years are those divisible by 4, but not those divisible by 100,
+//except that those divisble by 400 *are* leap years.
+//See Kernighan & Ritchie, 2nd edition, section 2.5.
+boolean isLeap(int y)
+{
+  return (y % 4 == 0 && y % 100 != 0) || y % 400 == 0;
+}
+
+
+
+
+
+
+
+
